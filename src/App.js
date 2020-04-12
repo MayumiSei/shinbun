988781a3a37c030a9555d2334e5a42d8c5b15c14
Chloé Moulin <chloee.moulin@gmail.com>
2020-04-12 12:59:48 +0200

    Création Home et Header components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import logo from './logo.svg';
+import './Assets/style/index.scss';
+import './App.scss';
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Header />
+			{/* <div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+           			</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+          			</a>
+				</header>
+			</div> */}
+			<Switch>
+				<Route exact path="/" component={Home} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Assets/style/index.scss';
+
+const Header = (props) => {
+    return(
+        <header>
+            <div className="container">
+                <h1>Shinbun</h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
