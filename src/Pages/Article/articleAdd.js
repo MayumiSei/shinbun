b1a277c0d6bfba35685b7bdaf2caa5b36ca2eda2
Chloé Moulin <chloee.moulin@gmail.com>
2020-04-13 15:47:45 +0200

    tinymce in ArticleAdd page

--- src/Pages/Article/articleAdd.js
+++ src/Pages/Article/articleAdd.js
@@ -4,17 +4,46 @@ import { withFirebase } from '../../Components/Firebase';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../Routes';
 import { AuthUserContext, withAuthorization} from '../../Components/Session';
+import { Editor } from '@tinymce/tinymce-react';
 
-const articleAdd = (props) => {
+const init = {
+    height: 500,
+    menubar: false,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste code help wordcount',
+        'code'
+    ],
+    toolbar: 'undo redo | formatselect | bold italic forecolor backcolor | \
+              alignleft aligncenter alignright alignjustify | \
+              bullist numlist outdent indent | removeformat | help | code'
+}
+
+const ArticleAdd = (props) => {
+    const handleEditorChange = (content, editor) => {
+        console.log('Content was updated:', content);
+    }
+
     return(
         <AuthUserContext.Consumer>
             {
                 authUser =>
-                    (authUser && authUser.role === "ADMIN") ?
-                    <div>
-                        <h1>Article Add</h1>
-                    </div> :
-                    <p style={ { color: 'black'} }>Vous ne pouvez pas accéder à cette page</p>
+                <div className="container">
+                    {
+                        (authUser && authUser.role === "ADMIN") ?
+                        <div>
+                            <h1>Article Add</h1>
+
+                            <form onSubmit="">
+
+                                <Editor initialValue="<p>This is the initial content of the editor</p>" init={ init } onEditorChange={handleEditorChange} />
+
+                            </form>
+                        </div> :
+                        <p style={ { color: 'black'} }>Vous ne pouvez pas accéder à cette page</p>
+                    }
+                </div>
 
 
             }
@@ -25,4 +54,4 @@ const articleAdd = (props) => {
 
 const condition = authUser => authUser && authUser.role === "ADMIN";
 
-export default withAuthorization(condition)(articleAdd);
+export default withAuthorization(condition)(ArticleAdd);
